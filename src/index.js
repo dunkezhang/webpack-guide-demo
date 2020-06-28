f4f5eec9f4d66fc66df8b3e629a8f9e8d13fe3c5
zhangdunke <dunkezhang@clubfactory.com>
2020-06-28 11:16:54 +0800

    feat(index.js): add component methods

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import _ from 'lodash'
+function component() {
+  const element = document.createElement('div')
+  element.innerHTML = _join(['Hello', 'webpack'], ' ')
+  return element
+}
+
+document.body.appendChild(component())
